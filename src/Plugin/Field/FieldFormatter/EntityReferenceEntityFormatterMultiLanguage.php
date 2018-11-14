b0cde33c22f368d791a1f38e756fa8f9e9c1ad80
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-11-14 14:24:28 +0100

    Update selection logic.

--- src/Plugin/Field/FieldFormatter/EntityReferenceEntityFormatterMultiLanguage.php
+++ src/Plugin/Field/FieldFormatter/EntityReferenceEntityFormatterMultiLanguage.php
@@ -2,62 +2,42 @@
 
 namespace Drupal\entity_reference_formatter_multilanguage\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
 use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceEntityFormatter;
 use Drupal\Core\TypedData\TranslatableInterface;
-use Drupal\language\Entity\ConfigurableLanguage;
-use Drupal\node\NodeInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\social_media_links\Plugin\SocialMediaLinks\Platform\Drupal;
 
 /**
  * Plugin implementation of the 'entity reference rendered entity' formatter.
  *
  * @FieldFormatter(
  *   id = "entity_reference_entity_view_multilanguage",
- *   label = @Translation("Rendered published entity in current language"),
- *   description = @Translation("Display the referenced entities rendered by entity_view()."),
- *   field_types = {
+ *   label = @Translation("Only render published entities in the current language"),
+ *   description = @Translation("Display the referenced entities rendered by
+ *   entity_view()."), field_types = {
  *     "entity_reference"
  *   }
  * )
  */
 class EntityReferenceEntityFormatterMultiLanguage extends EntityReferenceEntityFormatter {
+
   protected function getEntitiesToView(EntityReferenceFieldItemListInterface $items, $langcode) {
-    $origin_langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
-    $language = ConfigurableLanguage::load($origin_langcode);
-    
     $entities = parent::getEntitiesToView($items, $langcode);
+    $current_language = \Drupal::languageManager()->getCurrentLanguage();
     foreach ($entities as $delta => $entity) {
-      $published = TRUE;
-      $same_language = TRUE;
-      if ($fallback != "" && $entity->hasTranslation($fallback)) {
-        $langcode = $fallback;
-      }
-      else {
-        $langcode = $origin_langcode;
-      }
-      if ($entity instanceof NodeInterface) {
-        $same_language = $entity->hasTranslation($langcode);
-        $has_origin_language = $entity->hasTranslation($origin_langcode);
-        if($same_language){
-          $entity = $entity->getTranslation($langcode);
-          if ($has_origin_language) {
-            $entity = $entity->getTranslation($origin_langcode);
-          }
-          $entities[$delta] = $entity;
-        }
-        if ($has_origin_language) {
-          $published = $entity->getTranslation($origin_langcode)->isPublished();
-        } else {
-          $published = $entity->isPublished();
-        }
-      }
-      if (!$published || !$same_language) {
+      // Hide entities in differnt language.
+      if ($entity->language() != $current_language) {
         unset($entities[$delta]);
       }
-    }
 
+      // Hide non published entities. isPublished is part of trait.
+      // So we need to check the
+      if (method_exists($entity, 'isPublished') && !$entity->isPublished()) {
+        unset($entities[$delta]);
+      }
+
+    }
     return $entities;
   }
-
 }
